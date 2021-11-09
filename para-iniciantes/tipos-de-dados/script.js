040fdd186c2a37a85e6ec82c9aa2ca9be537bcaf
Matheus Ribeiro <matheus.ribeiro@farmaciaindiana.far.br>
2021-11-09 16:03:51 -0300

    Class about variables is completed

---
+++ para-iniciantes/tipos-de-dados/script.js
@@ -0,0 +1,12 @@
+/*7 TIPOS DE DADOS
+Todos são primitivos exceto os objetos.*/
+
+var nome = "André"; // String
+var idade = 28; // Number
+var possuiFaculdade = true; // Boolean
+var time; // Undefined
+var comida = null; // Null
+var simbolo = Symbol(); // Symbol
+var novoObjeto = {}; // Object
+
+// Os primitivos são dados imutáveis
