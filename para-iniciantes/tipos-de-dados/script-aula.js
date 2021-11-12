e2cedfd8b057f7b493d00d121df297d5a7f38708
Matheus Ribeiro <matheus.ribeiro@farmaciaindiana.far.br>
2021-11-11 21:29:20 -0300

    Class about data types is completed

---
+++ para-iniciantes/tipos-de-dados/script-aula.js
@@ -0,0 +1,45 @@
+/*7 TIPOS DE DADOS
+Todos são primitivos exceto os objetos.*/
+
+var nome = "André"; // String
+var idade = 28; // Number
+var possuiFaculdade = true; // Boolean
+var time; // Undefined
+var comida = null; // Null
+var simbolo = Symbol(); // Symbol
+var novoObjeto = {}; // Object
+
+// Os primitivos são dados imutáveis
+
+//Verificando o tipo do dado
+console.log(typeof nome);
+console.log(typeof idade);
+console.log(typeof possuiFaculdade);
+console.log(typeof time);
+console.log(typeof comida);
+console.log(typeof simbolo);
+console.log(typeof novoObjeto);
+console.log(typeof null); //object
+
+//STRING
+//Você pode somar uma string e assim concatenar as palavras.
+
+var nome = "Matheus";
+var sobrenome = "Ribeiro";
+var nomeCompleto = nome + " " + sobrenome;
+
+//ASPAS DUPLAS, SIMPLES E TEMPLATE STRING
+('JavaScript é "super" fácil');
+("JavaScript é 'super' fácil");
+('JavaScript é "super" fácil');
+`JavaScript é "super" fácil"`;
+//"JavaScript é "super" fácil"; // Inválido
+//COPIAR
+//Não necessariamente precisamos atribuir valores a uma variável
+
+// TEMPLATE STRING
+var gols = 1000;
+var frase1 = "Romário fez " + gols + " gols";
+var frase2 = `Romário fez ${gols} gols`; // Utilizando Template String
+// COPIAR
+// Você deve passar expressões / variáveis dentro de ${}
--- para-iniciantes/tipos-de-dados/script.js
+++ para-iniciantes/tipos-de-dados/script.js
@@ -1,12 +1,24 @@
-/*7 TIPOS DE DADOS
-Todos são primitivos exceto os objetos.*/
+//EXERCÍCIO
+//Dica rápida, frases (string) devem ser colocadas entre aspas (simples ou duplas) e números não levam aspas.
 
-var nome = "André"; // String
-var idade = 28; // Number
-var possuiFaculdade = true; // Boolean
-var time; // Undefined
-var comida = null; // Null
-var simbolo = Symbol(); // Symbol
-var novoObjeto = {}; // Object
+// Declare uma variável contendo uma string
+let stringVar = "String";
 
-// Os primitivos são dados imutáveis
+// Declare uma variável contendo um número dentro de uma string
+let stringNumber = "2";
+
+// Declare uma variável com a sua idade
+let myAge = 21;
+
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+let firstName = "Matheus";
+let lastName = "Ribeiro";
+
+let myName = `${firstName} ${lastName}`;
+
+// Coloque a seguinte frase em uma variável: It's time
+let text = "It's time";
+
+// Verifique o tipo da variável que contém o seu nome
+console.log(typeof myName);
