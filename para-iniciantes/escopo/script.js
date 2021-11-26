0cb4f08843ee96c3f20511c1a5141838aa61cba5
Matheus Ribeiro <matheus.ribeiro@farmaciaindiana.far.br>
2021-11-26 12:07:51 -0300

    The class about 'escopo' is completed

---
+++ para-iniciantes/escopo/script.js
@@ -0,0 +1,31 @@
+// Por qual motivo o código abaixo retorna com erros?
+{
+  var cor = "preto";
+  const marca = "Fiat";
+  let portas = 4;
+}
+//console.log(var, marca, portas);
+//var é uma palavra reservada
+//portas e marca só existe no escopo do bloco acima
+
+// Como corrigir o erro abaixo? corrigido
+const dois = 2;
+
+function somarDois(x) {
+  return x + dois;
+}
+function dividirDois(x) {
+  return x + dois;
+}
+somarDois(4);
+dividirDois(6);
+
+// O que fazer para total retornar 500? mudar pra 'let' no for
+var numero = 50;
+
+for (let numero = 0; numero < 10; numero++) {
+  console.log(numero);
+}
+
+const total = 10 * numero;
+console.log(total);
