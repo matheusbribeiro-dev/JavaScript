4db419df6da21987b42297cb84f24691251e9413
Matheus Ribeiro <matheus.ribeiro@farmaciaindiana.far.br>
2021-11-26 09:56:53 -0300

    The class 'tudo é objeto' is completed

---
+++ para-iniciantes/tudo-e-objeto/script.js
@@ -0,0 +1,17 @@
+//EXERCÍCIO
+// nomeie 3 propriedades ou métodos de strings
+let myName = "Matheus";
+//length
+//replace
+//split
+
+// nomeie 5 propriedades ou métodos de elementos do DOM
+
+//querySelector
+//addEventListener
+//append
+//createAttribute
+//firstChild
+
+// busque na web um objeto (método) capaz de interagir com o clipboard,
+// clipboard é a parte do seu computador que lida com o CTRL + C e CTRL + V
