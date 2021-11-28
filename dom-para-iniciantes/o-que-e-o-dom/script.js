396ac1fccf87860177cdbd3e81ca8e693eadd1eb
Matheus Ribeiro <matheus.ribeiro@farmaciaindiana.far.br>
2021-11-28 18:03:00 -0300

    The class about 'o que é o DOM' is completed

---
+++ dom-para-iniciantes/o-que-e-o-dom/script.js
@@ -0,0 +1,13 @@
+//EXERCÍCIO
+// Retorne o url da página atual utilizando o objeto window
+console.log(window.location.href);
+
+// Seleciona o primeiro elemento da página que
+// possua a classe ativo
+document.querySelector(".ativo");
+
+// Retorne a linguagem do navegador
+console.log(window.navigator.language);
+
+// Retorne a largura da janela
+window.innerWidth;
