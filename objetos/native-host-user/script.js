7d07f373332360162cafbede8f172ad34d1bf603
Matheus Ribeiro <matheusbrdev@gmail.com>
2022-01-03 10:39:55 -0300

    The class about 'native, host e user' is completed

---
+++ objetos/native-host-user/script.js
@@ -0,0 +1,18 @@
+// Liste 5 objetos nativos
+console.log(String);
+console.log(Object);
+console.log(Array);
+console.log(Number);
+console.log(Function);
+
+// Liste 5 objetos do browser
+console.log(Document);
+console.log(NodeList);
+console.log(HTMLAllCollection);
+console.log(Window);
+console.log(Location);
+
+// Liste 2 Métodos, Propriedades ou Objetos
+// presentes no Chrome que não existem no Firefox
+
+//Qualquer propriedade que comece com 'webkit'
