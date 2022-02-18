03a6165e1c5e1f83f31a102ba3dd79539ad6de5e
Matheus Ribeiro <matheusbrdev@gmail.com>
2022-02-18 10:33:54 -0300

    The tests is removed

--- efeitos-no-dom/modules/js/modules/accordion.js
+++ efeitos-no-dom/modules/js/modules/accordion.js
@@ -1,12 +1,10 @@
-import * as teste from './teste.js';
-
-console.log(teste.senha);
-
 export default function initAccordion() {
-  const accordionList = document.querySelectorAll('[data-anime="accordion"] dt');
-  const activeClass = 'ativo';
-  
-  if(accordionList.length) {
+  const accordionList = document.querySelectorAll(
+    '[data-anime="accordion"] dt'
+  );
+  const activeClass = "ativo";
+
+  if (accordionList.length) {
     accordionList[0].classList.add(activeClass);
     accordionList[0].nextElementSibling.classList.add(activeClass);
 
@@ -16,7 +14,7 @@ export default function initAccordion() {
     }
 
     accordionList.forEach((item) => {
-      item.addEventListener('click', activeAccordion);
+      item.addEventListener("click", activeAccordion);
     });
   }
 }
